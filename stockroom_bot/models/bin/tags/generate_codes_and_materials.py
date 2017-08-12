567c73cae0ad355f6ec6cdc6151940c7eba99df2
Takuji Kawata <takuji.kawata@gmail.com>
2017-08-13 01:30:30 +0900

    Fixing the runtime error on line 5
    Runtime error happens. Concatenation and %-formatting was applied to a text string which didn't work properly. 
    Removing the concatenation.

--- stockroom_bot/models/bin/tags/generate_codes_and_materials.py
+++ stockroom_bot/models/bin/tags/generate_codes_and_materials.py
@@ -2,8 +2,7 @@
 import os
 for i in xrange(0,12):
     os.system("rosrun ar_track_alvar createMarker %d" % i) #<1>
-    os.system("convert MarkerData_%d.png -bordercolor white -border 100x100 " +
-              "MarkerData_%d.png" % (i, i)) #<2>
+    os.system("convert MarkerData_%d.png -bordercolor white -border 100x100 MarkerData_%d.png" % (i, i)) #<2>
     with open("product_%d.material" % i, 'w') as f: #<3>
       f.write("""
 material product_%d {
