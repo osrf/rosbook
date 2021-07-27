20d4ab94d910adc62c4aecb471ceac13b5cef5ad
Amajd Majid <amjadmajid@users.noreply.github.com>
2021-07-27 19:11:57 +0200

    Update keys_to_twist_parameterized.py

--- teleop_bot/keys_to_twist_parameterized.py
+++ teleop_bot/keys_to_twist_parameterized.py
@@ -1,45 +1,44 @@
-#!/usr/bin/env python
-# BEGIN ALL
+#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
-key_mapping = { 'w': [ 0, 1], 'x': [0, -1], 
-                'a': [-1, 0], 'd': [1,  0], 
-                's': [ 0, 0] }
-g_last_twist = None 
-g_vel_scales = [0.1, 0.1] # default to very slow 
+g_last_twist = Twist()
+g_vel_scales = [.1, .1]
+key_mapping = {'w':[0,1], 'x':[0,-1], 
+               'a':[-1,0], 'd': [1,0], 
+               's':[0,0]}
 
 def keys_cb(msg, twist_pub):
-  global g_last_twist, g_vel_scales
-  if len(msg.data) == 0 or not key_mapping.has_key(msg.data[0]):
-    return # unknown key.
-  vels = key_mapping[msg.data[0]]
-  g_last_twist.angular.z = vels[0] * g_vel_scales[0]
-  g_last_twist.linear.x  = vels[1] * g_vel_scales[1]
-  twist_pub.publish(g_last_twist)
+    global  g_last_twist, g_vel_scales
 
-if __name__ == '__main__':
-  rospy.init_node('keys_to_twist')
-  twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-  rospy.Subscriber('keys', String, keys_cb, twist_pub)
-  g_last_twist = Twist() # initializes to zero
-  # BEGIN PARAM
-  if rospy.has_param('~linear_scale'):
-    g_vel_scales[1] = rospy.get_param('~linear_scale')
-  else:
-    rospy.logwarn("linear scale not provided; using %.1f" %\
-                  g_vel_scales[1])
-  # END PARAM
+    if len(msg.data) == 0 or not msg.data[0] in key_mapping: 
+        return
 
-  if rospy.has_param('~angular_scale'):
-    g_vel_scales[0] = rospy.get_param('~angular_scale')
-  else:
-    rospy.logwarn("angular scale not provided; using %.1f" %\
-                  g_vel_scales[0])
-
-  rate = rospy.Rate(10)
-  while not rospy.is_shutdown():
+    vels = key_mapping[msg.data[0]]
+    g_last_twist.angular.z = vels[0] * g_vel_scales[0]
+    g_last_twist.linear.x = vels[1] * g_vel_scales[1]
     twist_pub.publish(g_last_twist)
-    rate.sleep()
-# END ALL
+
+if __name__=='__main__':
+
+    rospy.init_node('key_to_tiwst')
+    twist_pub = rospy.Publisher('cmd_vel',Twist, queue_size=1)
+    rospy.Subscriber('keys', String, keys_cb, twist_pub)
+    rate = rospy.Rate(10)
+
+# BEGIN PARAM
+    if rospy.has_param('~linear_scale'):
+        g_vel_scales[1]=rospy.get_param('~linear_scale')
+    else:
+        rospy.logwarn('linear scale not provided; using {g_last_twist[1]}')
+        
+    if rospy.has_param('~angular_scale'):
+        g_vel_scales[0]=rospy.get_param('~angular_scale')
+    else:
+        rospy.logwarn('angular scale not provided; using {g_last_twist[0]}')
+# END ALL       
+    while not rospy.is_shutdown():
+        twist_pub.publish(g_last_twist)
+        rate.sleep()
+
